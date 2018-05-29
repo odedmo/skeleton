d26103500e04c26be03422eb7be2946ccff74020
odedmo <odedmoran222@gmail.com>
2018-05-29 21:58:30 +0300

    setup -> test

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    ]
+  }
+}
